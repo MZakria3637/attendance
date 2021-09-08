fa7f80a854aa129887ef5c54a842271ec9661421
muhammad zakria <zakriamuhammad3637@gmail.com>
2021-09-08 14:00:16 +0500

    2nd design

--- src/components/AttendTable.js
+++ src/components/AttendTable.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-
 // import { Link } from "react-router-dom";
 function AttendTable() {
     const [Attendees, setAttendees] = useState([]);
     const [Dat, setDate] = useState("");
+    const [id, setID] = useState("");
+    const [selectedName, setSelectedNAme] = useState({})
     useEffect(() => {
 
         fetch(
@@ -20,13 +21,41 @@ function AttendTable() {
         ).then((res) => res.json())
             .then((result) => {
                 setAttendees(result);
+                setSelectedNAme({});
             })
     }, [Dat])
 
+    useEffect(() => {
+        Attendees.map((Attendee) => {
+            if (id === Attendee.userId&&id!=="") {
+                setSelectedNAme(Attendee);
+                
+            }
+            else if(id==="")
+            {
+                setSelectedNAme({});
+                
+            }
+            return 2;
+
+        })
+
+    }, [id,Attendees])
     return (<>
         <div className="limiter">
             <div className="container-table100">
-                <input type="date" className="p-1" style={{ borderRadius: 3 }} data-date-format="dd/mm/yyyy" onChange={(e) => { setDate(new Date(e.target.value)) }} />
+                <label className="font-weight-bold">Order By Date</label>
+            <input type="date" className="p-1 m-2" style={{ borderRadius: 3 }} data-date-format="dd/mm/yyyy" onChange={(e) => { setDate(new Date(e.target.value)) }} />
+            <label className="font-weight-bold">Order By Name</label>
+                <select class="form-select w-25 m-2" aria-label="Default select example" onChange={(e) => { setID(e.target.value) }}>
+                    <option selected >Select Name</option>
+                    <option  value="">None</option>
+                    {Attendees.map((Attendee, index) => {
+                        return (
+                            <option key={index} value={Attendee.userId}>{Attendee.userName}</option>
+                        );
+                    })}
+                </select>
                 <div className="wrap-table100">
                     <div className="table100">
                         <table>
@@ -42,10 +71,25 @@ function AttendTable() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {Attendees.map((Attendee, index) => {
+                                {selectedName.userId?( <tr  id={selectedName.userId}>
+                                            <td className="column1">{selectedName.userName}</td>
+                                            <td className="column2">{selectedName.currentDate}</td>
+                                            <td className="column3">{selectedName.lastChkIn}</td>
+                                            <td className="column2">{selectedName.lastChkOut}</td>
+                                            <td className="column5">{selectedName.presence? "Present" : "Absent"}</td>
+                                            <td className="column5">{selectedName.totalTime} Hrs</td>
+                                            <td className="column6">
+                                                <p className="p-2">hello i made a website today hrfghjbf  hgdshf grsf rwhne f
+
+                                                </p>
+                                                {/* <Link className="btn btn-primary" to="/attendance/tasks">Show Details</Link> */}
+                                            </td>
+
+                                        </tr>):
+                                 Attendees.map((Attendee, index) => {
                                     return (
                                         <tr key={index} id={Attendee.userId}>
-                                            <td className="column1">{Attendee.userName}</td>
+                                            <td className="column1">{ Attendee.userName}</td>
                                             <td className="column2">{Attendee.currentDate}</td>
                                             <td className="column3">{Attendee.lastChkIn}</td>
                                             <td className="column2">{Attendee.lastChkOut}</td>
